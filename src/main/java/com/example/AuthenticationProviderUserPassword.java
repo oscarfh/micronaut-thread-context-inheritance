3b8c4b114e1f1615887bc0857c153546e43970a5
Oscar <3311764+oscarfh@users.noreply.github.com>
2020-08-10 10:32:50 -0300

    Micronaut 2.0.0 -> broken

--- src/main/java/com/example/AuthenticationProviderUserPassword.java
+++ src/main/java/com/example/AuthenticationProviderUserPassword.java
@@ -1,10 +1,14 @@
 package example.micronaut;
 
+import edu.umd.cs.findbugs.annotations.Nullable;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.security.authentication.AuthenticationException;
 import io.micronaut.security.authentication.AuthenticationFailed;
 import io.micronaut.security.authentication.AuthenticationProvider;
 import io.micronaut.security.authentication.AuthenticationRequest;
 import io.micronaut.security.authentication.AuthenticationResponse;
 import io.micronaut.security.authentication.UserDetails;
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import org.reactivestreams.Publisher;
 
@@ -15,11 +19,15 @@ import java.util.ArrayList;
 public class AuthenticationProviderUserPassword implements AuthenticationProvider  {
 
     @Override
-    public Publisher<AuthenticationResponse> authenticate(AuthenticationRequest authenticationRequest) {
-        if ( authenticationRequest.getIdentity().equals("sherlock") &&
-                authenticationRequest.getSecret().equals("password") ) {
-            return Flowable.just(new UserDetails((String) authenticationRequest.getIdentity(), new ArrayList<>()));
-        }
-        return Flowable.just(new AuthenticationFailed());
+    public Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest, AuthenticationRequest<?, ?> authenticationRequest) {
+        return Flowable.create(emitter -> {
+            if ( authenticationRequest.getIdentity().equals("sherlock") &&
+                    authenticationRequest.getSecret().equals("password") ) {
+                emitter.onNext(new UserDetails((String) authenticationRequest.getIdentity(), new ArrayList<>()));
+                emitter.onComplete();
+            } else {
+                emitter.onError(new AuthenticationException(new AuthenticationFailed()));
+            }
+        }, BackpressureStrategy.ERROR);
     }
 }
